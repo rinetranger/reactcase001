e9caa529b09cf95e568581519f2466530ec5279e
rinetranger <rinetranger@gmail.com>
2023-05-27 12:45:22 +0900

    socialmodify1.0

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,11 +23,11 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
+                <li>&copy; Copyright 2023 TeamRink</li>
                 <li>
                   Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
+                  <a title="TeamRink" href="">
+                    TeamRink
                   </a>
                 </li>
               </ul>
